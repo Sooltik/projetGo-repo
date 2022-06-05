3604ca3e6461334d5c8875d068a110333133f7aa
Mouloud Ould Ali <mouloudouldali78@gmail.com>
2022-06-04 23:35:32 -0400

    update

--- index.php
+++ index.php
@@ -30,10 +30,11 @@ if (in_array($page . '.func.php', $pages_functions)) {
 
     <link type="text/css" rel="stylesheet" href="css/materialize.css" media="screen,projection" />
 
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>GoFundMe-ProjetGo</title>
 
 
 
