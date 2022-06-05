4a8f2d6156d3dd0e247becdd717aa14faf7ba50a
Mouloud Ould Ali <mouloudouldali78@gmail.com>
2022-06-04 23:53:59 -0400

    update page post.func.php -just the post with 1

--- functions/post.func.php
+++ functions/post.func.php
@@ -15,6 +15,7 @@ function get_post()
         JOIN admins
         ON posts.writer = admins.email
         WHERE posts.id='{$_GET['id']}'
+        AND posts.posted = '1'
     ");
 
     $result = $req->fetchObject();
