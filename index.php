48efb49fbb2aa8289566ba269dd3576539320252
Mouloud Ould Ali <mouloudouldali78@gmail.com>
2022-06-09 12:08:28 -0400

    update pages

--- index.php
+++ index.php
@@ -52,12 +52,6 @@ if (in_array($page . '.func.php', $pages_functions)) {
         include 'pages/' . $page . '.php';
         ?>
     </div>
-
-
-
-
-
-
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 
 
