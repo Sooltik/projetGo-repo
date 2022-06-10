a50bbbe31b5c335377ed4416868017823dbb8769
Mouloud Ould Ali <mouloudouldali78@gmail.com>
2022-06-09 22:33:48 -0400

    update navbar

--- body/topbar.php
+++ body/topbar.php
@@ -7,10 +7,20 @@
       <a href="#" data-activates="mobile-menu" class="button-collapse"><i class="material-icons">menu</i></a>
       <ul class="right hide-on-med-and-down">
 
-        <li class="<?php echo ($page == "home") ? "active" : ""; ?>"><a href="index.php?page=home" style="font-size: 20px; font-weight:bold;">Home</a></li>
-        <li class="<?php echo ($page == "projet") ? "active" : ""; ?>"><a href="index.php?page=projet" style="font-size: 20px; font-weight:bold">Projets</a></li>
+        <li class="<?php echo ($page == "home") ? "active" : ""; ?>">
+          <a href="index.php?page=home" style="font-size: 20px; font-weight:bold;">Home</a>
+        </li>
 
-        <li class="<?php echo ($page == "login") ? "active" : ""; ?>"><a href="gestion/index.php?page=login" style="font-size: 25px; font-weight:bold"><i class="material-icons">cast_connected</i></a></li>Connexion</a></li>
+        <li class="<?php echo ($page == "projet") ? "active" : ""; ?>">
+          <a href="index.php?page=projet" style="font-size: 20px; font-weight:bold">
+            Projets</a>
+        </li>
+
+        <li class="<?php echo ($page == "login") ? "active" : ""; ?>">
+          <a href="gestion/index.php?page=login" style="font-size: 25px; font-weight:bold">
+            <i class="material-icons">cast_connected</i></a>
+        </li>Connexion</a>
+        </li>
 
 
 
