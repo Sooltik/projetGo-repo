8e9e1375744add570b237c5af1f53cb777c86f14
Mouloud Ould Ali <mouloudouldali78@gmail.com>
2022-06-13 09:33:48 -0400

    update validation page

--- gestion/body/topbar.php
+++ gestion/body/topbar.php
@@ -8,11 +8,12 @@
 
             <a href="#" data-activates="mobile-menu" class="button-collapse"><i class="material-icons">menu</i></a>
             <ul class="right hide-on-med-and-down">
-                <li class="<?php echo ($page == "gestion") ? "active" : ""; ?>"><a href="index.php?page=gestion" style="font-size: 25px; font-weight:bold"><i class="material-icons">dashboard</i></a></li>
+                <li class="<?php echo ($page == "gestion") ? "active" : ""; ?>"><a href="index.php?page=gestion" style="font-size: 25px; font-weight:bold"><i class="material-icons">format_list_numbered</i></a></li>
 
-                <li class="<?php echo ($page == "new_projet") ? "active" : ""; ?>"><a href="index.php?page=new_projet" style="font-size: 25px; font-weight:bold"><i class="material-icons">edit</i></a></li>
+                <li class="<?php echo ($page == "new_projet") ? "active" : ""; ?>"><a href="index.php?page=new_projet" style="font-size: 25px; font-weight:bold"><i class="material-icons">fiber_new
+                        </i></a></li>
 
-                <li class="<?php echo ($page == "liste_projet") ? "active" : ""; ?>"><a href="index.php?page=liste_projet" style="font-size: 25px; font-weight:bold"><i class="material-icons">view_list</i></a></li>
+                <li class="<?php echo ($page == "liste_projet") ? "active" : ""; ?>"><a href="index.php?page=liste_projet" style="font-size: 25px; font-weight:bold"><i class="material-icons">format_align_justify</i></a></li>
 
                 <li><a href="../index.php?page=home"><i class="material-icons">exit_to_app</i></a>
 
